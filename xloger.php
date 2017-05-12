952c12f02adbe22de3402fba99a4cb9e3fb70071
huangjunlin <stone58@qq.com>
2017-05-12 17:07:45 +0800

    sql error

--- xloger.php
+++ xloger.php
@@ -412,13 +412,12 @@ class XLogerHelper {
 		if( !(XLoger::$TRACE_LOG & XLOGER_CUSTOM_SQL) || !$sqltrace instanceof XLogerSqlQueryTrace){return;}
 		if(!$this->_watched) return; 
 		$data = $sqltrace->traceData();
-		$this->trace( "sqlquery", $data );
 		if($data['error']){
 			$this->trace('error', array_merge($data, array(
 				"message"=> "{$data['error']} when SQL Excute[{$data['query']}]"
 			)));
 		}
-		return;
+		return $this->trace( "sqlquery", $data );
 	}
 
 	/**
